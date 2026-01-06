c5a580291e7587865b5eba00d3c340dc2acb4c5c
Alexandre ROUGET <alexandre.rouget@etu.minesparis.psl.eu>
2026-01-06 15:16:32 +0100

    création d'un graphe

--- TP-fourmis-main.py
+++ TP-fourmis-main.py
@@ -1,4 +1,6 @@
 import flet as ft
+import random as rd
+from math import *
 
 def main(page):
     page.title = "Algorithme de colonie de fourmis"
@@ -9,7 +11,8 @@ def main(page):
 
     
     bouton = ft.Button("Cliquez", on_click=lambda e: print("cliqué !"))
-    
+    createur = ft.Button("Générer le graphe", on_click=lambda e: generer_nodes())
+
     page.add(ft.Column([titre, bouton]))
 
     champ1 = ft.TextField(label = 'Nombre de noeuds', value = '20', width = 150)
@@ -21,6 +24,34 @@ def main(page):
 
     statut = ft.Text("Prêt à démarrer", color = 'green', size = 16)
 
-    page.add(ft.Column([ft.Text("Paramètres de l'algorithme", size = 16), ft.Row([champ1,champ2,champ3]), ft.Divider(), statut, zone]))
+    page.add(ft.Column([ft.Text("Paramètres de l'algorithme", size = 16), ft.Row([champ1,champ2,champ3]), createur, ft.Divider(), statut, zone]))
+
+    nodes = []
+    def generer_nodes():
+        try:
+            N = int(champ1.value)
+        except ValueError: N=20
+        for _ in range(N):
+            x = rd.uniform(50,550)
+            y = rd.uniform(50,450)
+            nodes.append((x,y))
+        dessiner_graphe()
+    
+    def dessiner_graphe():
+        shapes = []
+        for i in range(len(nodes)) :
+            x,y = nodes[i]
+            noeud = ft.Container(border_radius = 10,
+                                 height = 20,
+                                 width = 20,
+                                 left = x-10,
+                                 top = y-10,
+                                 content = ft.Text(f"{i}"),
+                                 bgcolor = "green")
+            shapes.append(noeud)
+        zone.content = ft.Stack(controls = shapes, 
+                                width = 600, height = 500)
+        page.update()
+
 
 ft.run(main)
